ed3b51e2eb37426a4824af25dac08d42c16679ac
FloreauLuca <lucafloreau@akeonet.com>
2021-07-09 16:23:13 +0200

    Init champion

---
+++ StrategieTest/StrategieTest.cpp
@@ -0,0 +1,12 @@
+
+#include "interface.h"
+
+int main()
+{
+	partie_init();
+	for (int i = 0; i < NB_TOURS; ++i)
+	{
+		jouer_tour();
+	}
+	partie_fin();
+}
---
+++ StrategieTest/champion.cc
@@ -0,0 +1,26 @@
+#include "interface.h"
+
+#include <iostream>
+
+#include "api.hh"
+
+/// Fonction appelée au début de la partie.
+void partie_init()
+{
+    std::cout << "partie_init\n";
+    // TODO
+}
+
+/// Fonction appelée à chaque tour.
+void jouer_tour()
+{
+    std::cout << "jouer_tour\n";
+    // TODO
+}
+
+/// Fonction appelée à la fin de la partie.
+void partie_fin()
+{
+    std::cout << "partie_fin\n";
+    // TODO
+}
